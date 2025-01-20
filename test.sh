9194911262d6f0950cb3e3ef748e815d8c6ba2a1
rahul-presidio <rprabhu@presidio.com>
2025-01-20 13:57:13 +0530

    Artifact

--- test.sh
+++ test.sh
@@ -1,7 +1,1 @@
-# params=$(cat ${{ inputs.parameterFilePath }})
-params=$(cat .parameters/hub.non-prod.parameters.json )
-orgId=$(echo $params | jq .parameters.organizationId.value -r)
-env=$(echo $params | jq .parameters.environment.value -r)
-resourceNumber=$(echo $params | jq .parameters.resourceNumber.value -r)
-rgName=$(printf rg-$orgId-$env-%03d $resourceNumber)
-echo $rgName
+hello
